45bb3a2af588658369503e7e6fb37f894686049f
patrick095 <patrick095@gmail.com>
2021-07-24 21:15:11 -0300

    add personal projects in portifolio

--- pages/portifolio.js
+++ pages/portifolio.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
+import Image from 'next/image'
 import { createGitInstance } from "../services/api"
 import styles from '../styles/Portifolio.module.css'
+import LinkIcon from '@material-ui/icons/Link'
 
 
 export default function Portifolio(){
@@ -11,21 +13,62 @@ export default function Portifolio(){
         .then(res => setRepos(res.data))
     },[])
 
-    return (<div className={styles.Portifolio}>
-        <h1>Repositórios públicos no Github</h1>
-        {repos.map(repo =>{
-            let updated = new Date(repo.updated_at)
-            let created = new Date(repo.created_at)
-            let forked = ''
-            if (repo.fork) {
-                forked = "(fork)"
-            }
-            return <a href={repo.html_url} target="_blank">{
-                repo.name}
-                <span style={{color: '#e59f16'}}>{' ('+created.getFullYear()+'-'+updated.getFullYear()+') '
-                }</span>
-                <span style={{color: '#000'}}>{forked}</span>
+    function handlePersonalProject(id){
+        if ( !Array.from(document.querySelector('#'+id).classList).find(element => element === styles.personalProjectD)) {
+            return document.querySelector('#'+id).classList.add(styles.personalProjectD)
+        }
+        let projects = Array.from(document.querySelectorAll('.'+styles.personalProject))
+        projects.map(project => project.classList.add(styles.personalProjectD))
+        document.querySelector('#'+id).classList.toggle(styles.personalProjectD)
+    }
+
+    return (
+    <div className={styles.Portifolio}>
+        <div className={styles.gitList}>
+            <h1>Repositórios Github</h1>
+            {repos.map(repo =>{
+                let updated = new Date(repo.updated_at)
+                let created = new Date(repo.created_at)
+                let forked = ''
+                if (repo.fork) {
+                    forked = "(fork)"
+                }
+                return <a key={repo.html_url} href={repo.html_url} target="_blank">{
+                    repo.name}
+                    <span style={{color: '#000'}}>{forked}</span>
+                    </a>
+            })}
+        </div>
+        <div className={styles.personalList}>
+            <h1>Projetos pessoais</h1>
+            <div id="placarVolei" className={styles.personalProject+' '+styles.personalProjectD}>
+                <h2 onClick={() => handlePersonalProject("placarVolei")}>Placar Volei</h2>
+                <a href="https://placarvolei.com.br" target="_blank">
+                    Link <LinkIcon />
                 </a>
-        })}
+                <h4>Descrição:</h4>
+                <span>Projeto criado para poder de forma fácil adicionar um placar na transmissão de jogo de vôlei, assim como exibir no telão, tudo no mesmo lugar</span>
+                <h4>Tecnologias:</h4>
+                <span><strong>Front-end:</strong> ReactJs, NextJs, Typescript, axios, nookies, socket.io-client </span>
+                
+                <span><strong>Back-end:</strong> NodeJs, Express, socket.io, mongoose, jwt, bcryptjs, nodemailer, MercadoPago</span>
+                
+                <span><strong>Banco de dados:</strong> MongoDB</span>
+            </div>
+            <div id="univolei" className={styles.personalProject+' '+styles.personalProjectD}>
+                <h2 onClick={() => handlePersonalProject("univolei")}>Univolei</h2>
+                <a href="https://univolei.com.br" target="_blank">
+                    Link <LinkIcon />
+                </a>
+                <h4>Descrição:</h4>
+                <span>Site criado para meu time local de voleibol.</span>
+                <h4>Tecnologias:</h4>
+                <span><strong>Front-end:</strong> ReactJs, axios, styled-components, nuka-carousel </span>
+                
+                <span><strong>Back-end:</strong> NodeJs, Express, mongoose, jwt, bcryptjs</span>
+                
+                <span><strong>Banco de dados:</strong> MongoDB</span>
+            </div>
+        </div>
     </div>)
 }
