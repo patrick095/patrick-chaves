f937a495fdd84442c1159387a16e65c5dbf63bb8
patrick095 <patrick095@gmail.com>
2021-07-27 19:09:27 -0300

    fix locale to 'pt-br'

--- next.config.js
+++ next.config.js
@@ -2,5 +2,15 @@ require('dotenv').config()
 module.exports = {
     env: {
         TOKEN: process.env.TOKEN
+    },
+    i18n: {
+        locales: ['pt-br'],
+        defaultLocale: 'pt-br',
+        domains: [
+            {
+                domain: 'patrickchaves.com.br',
+                defaultLocale: 'pt-br'
+            }
+        ]
     }
 }
