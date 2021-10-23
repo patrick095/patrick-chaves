6cea3a35887f3af6c3f172d6cb3549f56eaeca03
patrick095 <patrick095@gmail.com>
2021-10-23 14:18:42 -0300

    alteração da rota do gitlanguage para /api/gitlanguagge

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,8 @@
 import axios, { AxiosResponse } from 'axios'
 import { IGitLanguagesResponse } from '../interfaces/services.interfaces';
 
-export function createAPiInstance(){
-    return axios.create({
-        baseURL: '/api'
-    })
-}
-
 export function getGitLanuages(): Promise<IGitLanguagesResponse> {
-    const api = createAPiInstance();
-    return api.get("?user=patrick095").then((res: AxiosResponse<any>) => {
+    return axios.get("/api/gitlanguage?user=patrick095").then((res: AxiosResponse<any>) => {
         let keys = Object.keys(res.data);
         return {
             codingLanguages: res.data,
